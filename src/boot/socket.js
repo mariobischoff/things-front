fbb280bd67edc39436ab720f61fe934eb6f28164
Thalles Sobral <thazsobral@gmail.com>
2019-11-23 16:05:48 -0300

    configuração para servidor heroku

--- src/boot/socket.js
+++ src/boot/socket.js
@@ -3,6 +3,6 @@ import VueSocketIO from 'vue-socket.io'
 export default ({ Vue }) => {
   Vue.use(new VueSocketIO({
     debug: true,
-    connection: 'http://localhost:3000'
+    connection: process.env.API
   }))
 }
