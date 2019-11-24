88f0909e142d5259e005c6c527a8af89c5681734
Mario Bischoff <mariobischoffneto@gmail.com>
2019-11-24 10:50:37 -0300

    Alteração no Control para que tambem seja possivel enviar o estado Auto.

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="q-pa-md" style="max-width: 100%">
-    <q-toggle v-model="auto" label="Automático" />
+    <q-toggle v-model="auto" label="Automático"/>
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -27,8 +27,8 @@
       </q-list>
 
       <div>
-        <q-btn label="Enviar" type="submit" color="primary" :disable=auto />
-        <q-btn label="Restaurar" type="reset" color="primary" flat class="q-ml-sm" :disable=auto />
+        <q-btn label="Enviar" type="submit" color="primary"/>
+        <q-btn label="Restaurar" type="reset" color="primary" flat class="q-ml-sm"/>
       </div>
     </q-form>
 
@@ -45,25 +45,25 @@ export default {
       pump: false
     }
   },
-
   methods: {
     onSubmit () {
-      if (this.cooler !== true && this.pump !== true) {
-        this.$q.notify({
-          color: 'red-5',
-          textColor: 'white',
-          icon: 'warning',
-          position: 'top',
-          message: 'Comandos não alterados'
-        })
-      } else {
-        this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Comandos enviados'
-        })
-      }
+      // if (this.cooler !== true && this.pump !== true) {
+      //   this.$q.notify({
+      //     color: 'red-5',
+      //     textColor: 'white',
+      //     icon: 'warning',
+      //     position: 'top',
+      //     message: 'Comandos não alterados'
+      //   })
+      // } else {
+      this.$socket.emit('setConfig', { automatic: this.auto, cooler: this.cooler, pump: this.pump })
+      this.$q.notify({
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'Comandos enviados'
+      })
+      // }
     },
     onReset () {
       this.cooler = false
